5f23cc89b0a8e2e7546443382d2bd357f373d88b
gakkunn <yoshizumi.free01@gmail.com>
2023-08-29 15:32:03 +0900

    [add]Blockの画面遷移を追加
    blocks_controllerのアクションや、ビューを追加した

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,22 +1,52 @@
 class BlocksController < ApplicationController
+    before_action :set_block, only: %i[ show edit update destroy ]
+
     def index
+        @blocks = current_user.blocks
     end
   
     def show
     end
   
     def new
+        @block = current_user.blocks.build
     end
   
     def create
+        @block = current_user.blocks.build(block_params)
+
+        if @block.save
+            redirect_to blocks_path, notice: 'Block and URL were successfully created.'
+        else
+            render :new
+        end
     end
   
     def edit
     end
   
     def update
+        if @block.update(block_params)
+            redirect_to blocks_path, notice: 'Block and URL were successfully updated.'
+        else
+            render :edit
+        end
     end
   
     def destroy
+        if @block.destroy
+          redirect_to blocks_path, notice: 'Block was successfully deleted.'
+        else
+          redirect_to blocks_path, alert: 'Failed to delete the block.'
+        end
+    end
+
+    private
+    def set_block
+        @block = current_user.blocks.find(params[:id])
+    end
+
+    def block_params
+      params.require(:block).permit(:name)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'application#hello'
+  root 'static_pages#home'
   resources :blocks
 end

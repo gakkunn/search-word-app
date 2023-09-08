46a25bb07d763fe577d1162f22652f690f9be936
gakkunn <yoshizumi.free01@gmail.com>
2023-09-08 17:53:21 +0900

    [add]モデルテストの追記

--- test/models/block_test.rb
+++ test/models/block_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class BlockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @block = blocks(:example_block)
+  end
+
+  test "name should be present" do
+    @block.name = nil
+    assert_not @block.valid?
+  end
+
+  test "name should be unique" do
+    duplicate_block = @block.dup
+    @block.save
+    assert_not duplicate_block.valid?
+  end
+
+  test "name should not be too long" do
+    @block.name = "a" * 31
+    assert_not @block.valid?
+  end
+
+  test "user_id should be present" do
+    @block.user_id = nil
+    assert_not @block.valid?
+  end
 end
--- test/models/urlset_test.rb
+++ test/models/urlset_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class UrlsetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @urlset = urlsets(:example_urlset)
+  end
+
+  test "name should be present if address is present" do
+    @urlset.name = nil
+    assert_not @urlset.valid?
+  end
+
+  test "address should be present if name is present" do
+    @urlset.address = nil
+    assert_not @urlset.valid?
+  end
+
+  test "address should be valid URL" do
+    @urlset.address = "invalidURL"
+    assert_not @urlset.valid?
+  end
 end

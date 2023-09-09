8819c5b560fcf7092b4d88d992b7d2ef70d0a6f5
gakkunn <yoshizumi.free01@gmail.com>
2023-09-09 15:33:50 +0900

    [remove]forget your passwordの機能を削除
    ビュー、ルーティング、モジュールから削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: [:passwords]
   root 'blocks#index'
   resources :blocks
   get '/healthcheck', to: 'healthcheck#index'
